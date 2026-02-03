aa5cc8985fdb763f8c6e83fcdcb53ec5a491ebe6
yavuzkaramert <96007336+yavuzkaramert@users.noreply.github.com>
2026-02-03 09:38:11 +0300

    test1

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Welcome to the Flask app!"})
+    return jsonify({"message": "Welcome to the Flask app! :D"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
